499124fdce5a6dcfeaf7c827345a882934200347
BM Advisors <corretora.bmadvisors@gmail.com>
2026-07-16 22:05:20 -0300

    v0.1.3: inclui camera_web no pacote (registrador regenerado apos flutter clean)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2259709135" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1749233331" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
